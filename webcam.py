6d3f6bbb5c0f1f320d5e9dd8a2751b240cff7b26
Joseph Linares <joseph15300142@gmail.com>
2023-05-17 10:04:57 -0600

    Added -d for videodevice, -p for period in ms and -n for the number of samples

--- webcam.py
+++ webcam.py
@@ -1,19 +1,41 @@
 import cv2
+import sys
+import time
 
-camera = cv2.VideoCapture(0)
+device = 0
+period = 1000
+number = 10
+
+for index, argument in enumerate(sys.argv):
+    if argument in ("-d", "--device"):
+        device = int(sys.argv[index + 1])
+    elif argument in ("-p", "--period"):
+        period = int(sys.argv[index + 1])
+    elif argument in ("-n", "--number"):
+        number = int(sys.argv[index + 1])
+
+camera = cv2.VideoCapture(device)
 
 # Check if the webcam is opened correctly
 if not camera.isOpened():
     raise IOError("Cannot open webcam")
 
-while True:
+filename = "files/IMG_"
+filecount = 0
+fileextension = ".jpg"
+period /= 1000
+
+while filecount < number:
     ret, frame = camera.read()
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
 
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
+    temporal_filename = filename + str(filecount) + fileextension
+    cv2.imwrite(temporal_filename, frame)
+
+    time.sleep(period)
+    filecount += 1
+    cv2.waitKey(1)
 
 camera.release()
 cv2.destroyAllWindows()
